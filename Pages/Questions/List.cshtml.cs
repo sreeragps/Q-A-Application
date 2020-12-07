f225223d4917404f3cc35be4971075b0fffb733c
sreeragps <sreerag@brainchild.services>
2020-12-07 13:01:44 +0530

    Created question list.

--- Pages/Questions/List.cshtml.cs
+++ Pages/Questions/List.cshtml.cs
@@ -5,20 +5,40 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
 
 namespace StackOverFlowProject.Pages
 {
-    public class ListModel : PageModel
+      public class ListModel : PageModel
     {
-        private readonly ILogger<ListModel> _logger;
-
-        public ListModel(ILogger<ListModel> logger)
-        {
-            _logger = logger;
-        }
-
+        public List<QuestionsDomain> QuestionList = new List<QuestionsDomain>();
         public void OnGet()
         {
+            SqlConnection connection = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB;Initial Catalog=StackOverflow;Integrated Security=True");
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT * FROM stack", connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+               QuestionList.Add(new QuestionsDomain(reader.GetString(1), reader.GetString(2)));
+            }
         }
+    }
+    public class QuestionsDomain
+    {
+        public QuestionsDomain() { }  
+        public QuestionsDomain(string question, string details)
+        {
+            this.Questions = question;
+            this.Details = details;
+            
+        }
+        public string Questions { get; private set; } 
+        public string Details{ get; private set; }
+        
+       /* public override string ToString()  
+        {
+            return " Movie Name:" + this.MovieName + "\n Description:" + this.Synopsis + "\n Release Year:" + this.Year;
+        }*/
     }
 }
